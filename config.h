65090394b79330555857a671987ec76b41db3a02
F4AVI Fabrice <101743242+F4AVI@users.noreply.github.com>
2022-04-23 08:32:28 +0200

    Add files via upload

---
+++ config.h
@@ -0,0 +1,12 @@
+// Configuration
+
+// General setting
+int SF = 7;                  // Spreading Factor 12 -> for LoRa-APRS
+int TX_OUTPUT_POWER = 20;     // Power maxi 20 dBm
+
+// User setting
+String Call = "F4AVI-7";      // Call Sign of the operator
+String BEACON_SYMBOL = "[";   // Caractère définissant le symbôle affiché sur aprs.fi 
+                              // "[" -> Runner
+                              // "<" -> Motocycle
+                              // ">" -> Car
